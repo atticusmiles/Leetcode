74e37f5e969bd48f4d152a5f892a7d43c77cdb2a
Desmond <howfun123@163.com>
2015-05-05 21:28:51 +0800

    205

--- Leet_205_easy/205main.cpp
+++ Leet_205_easy/205main.cpp
@@ -1,10 +1,18 @@
 #include <string>
+#include <iostream>
 using namespace std;
 
 class Solution {
 public:
-    bool isIsomorphic(string s, string t) {
-        
+    bool isIsomorphic(string s, string t) 
+    {
+    	return true;    
     }
 };
+
+int main()
+{
+	cin.get();
+	return 0;
+}
 
