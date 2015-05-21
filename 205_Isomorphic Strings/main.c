166008ee794e1b9f5b3ba6c27f025d1d7c63045d
Desmond <howfun123@163.com>
2015-05-21 23:51:09 +0800

    205测试

--- 205_Isomorphic Strings/main.c
+++ 205_Isomorphic Strings/main.c
@@ -1,7 +1,52 @@
-#include <stdbool.h>
+//
+//  main.c
+//  Test
+//
+//  Created by Desmond on 15-5-21.
+//  Copyright (c) 2015年 Desmond. All rights reserved.
+//
 
-bool isIsomorphic(char* s, char* t) 
+#include <stdbool.h>
+#include <stdio.h>
+bool isIsomorphic(char* s, char* t)
 {
-	
-    return 1;
+    int Cypher[256];
+    
+    for (int i=0; i<256; ++i)
+    {
+        Cypher[i]=-1;
+    }
+    
+    while ('\0'!=*s)
+    {
+        if (-1==Cypher[(int)*s])
+        {
+            Cypher[(int)*s]=(int)*t;
+            ++s;
+            ++t;
+        }
+        else
+        {
+            if (*t!=Cypher[(int)*s])
+            {
+                return false;
+            }
+            else
+            {
+                ++s;
+                ++t;
+            }
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    char *s;
+    char *t;
+    s="abc";
+    t="abc";
+    printf("%i\n", isIsomorphic(s,t));
+    while(1);
 }
