2e6ded691f7530758a2ab552f9d5e7834755e6ef
Desmond <howfun123@126.com>
2015-07-02 15:41:57 +0800

    027 done.

--- 027_Remove_Element/027.cpp
+++ 027_Remove_Element/027.cpp
@@ -1,11 +1,23 @@
 #include <vector>
 using namespace std;
 
+
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) 
     {
-
-    	
+        auto iter = nums.begin();
+   		while(iter!=nums.end())
+   		{
+   			if(val == *iter)
+   			{
+   				nums.erase(iter);
+   			}
+   			else
+   			{
+   				++iter;
+   			}
+   		}
+		return nums.size();
     }
 };
