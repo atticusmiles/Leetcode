5bab29d52bf4a0d68133d5d3c2a58bc8b591c487
Desmond <howfun123@126.com>
2015-07-12 14:15:06 +0800

    016

--- 016_3Sum_Closest/16.cpp
+++ 016_3Sum_Closest/16.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <math.h>
 using namespace std;
 
 class Solution {
@@ -6,6 +7,17 @@ public:
     int threeSumClosest(vector<int>& nums, int target) 
     {
     	sort(nums.begin(),nums.end());
-    	
+
+    	int close_range = abs(nums[0]+nums[1]+nums[2]-target);
+    	for(auto iter=nums.begin(); iter<nums.end(); ++iter  )
+    	{
+    		int head=(iter=nums.begin());
+    		int rear=nums.size()-1;
+			
+			if()    		
+
+
+
+    	}
     }
 };
